9407216b675e87da6dbbf56042e4724979e272f7
9M2PJU <9m2pju@gmail.com>
2026-06-27 18:20:24 +0800

    Fix GitHub Pages deployment

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "emcomm-dashboard-v3";
+const cacheName = "emcomm-dashboard-v4";
 const assets = [
   "./",
   "./index.html",
@@ -9,12 +9,14 @@ const assets = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))))
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/9M2PJU-Amateur-Radio-EmComm-Dashboard/"
+  base: "/"
 });
